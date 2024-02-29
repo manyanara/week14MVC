70bb9ed7d2d31b6acae29101d21138361ec4cf70
Manya Naranzogt <manya.naranzogt@gmail.com>
2024-02-28 22:41:24 -0800

    added connection.js with comments

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+const Sequelize= require('sequelize');
+require('dotenv').config();  //import and configure dotenv
+
+let sequelize;
+
+//checks if theres a JAWSDB URL already, if not creates a new sequelize connection using dotenv access info and setting a location for the database
+if (process.env.JAWSDB_URL){
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host:'localhost',
+            dialect:'mysql',
+            port: 3006
+        }
+    );
+}
+
+module.exports= sequelize; 
