1c69080420dc55db7ac69f6319b5a2eecee487e6
Manya Naranzogt <manya.naranzogt@gmail.com>
2024-02-29 10:40:02 -0800

    added models

---
+++ models/Comment.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../confit/connection');
+
+class Comment extends Model {}
+
+Comment.init( 
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        body: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        date: {
+            
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            reference: {
+                model: 'post',
+                key: 'id'
+            }
+        },
+}, {
+    sequelize,
+    timestamp: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+})
